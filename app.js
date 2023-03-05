dc6666d7c2cbfd0fdac3415c76852c3bec03011b
nushankodikara <nushankodi@gmail.com>
2023-03-05 21:19:15 +0530

    Added audio on and off option and improved UI

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ async function startCapture(displayMediaOptions) {
             displayMediaOptions
         );
 
-        // Get audio stream from microphone
-        const audioStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-        });
+        let audio = document.getElementById("audio").checked;
 
-        // Add microphone audio track to screen capture stream
-        audioStream.getAudioTracks().forEach((track) => {
-            captureStream.addTrack(track);
-        });
+        if (audio) {
+            // Get audio stream from microphone
+            const audioStream = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+            });
+
+            // Add microphone audio track to screen capture stream
+            audioStream.getAudioTracks().forEach((track) => {
+                captureStream.addTrack(track);
+            });
+        }
     } catch (err) {
         console.error(`Error: ${err}`);
     }
@@ -24,12 +28,12 @@ async function startCapture(displayMediaOptions) {
     return captureStream;
 }
 
-recordButton.addEventListener("click", async () => {
+async function startRecording(audio) {
     let captureStream = await startCapture({
         video: {
             cursor: "always",
         },
-        audio: true,
+        audio,
     });
 
     let recorder = new MediaRecorder(captureStream);
@@ -58,9 +62,14 @@ recordButton.addEventListener("click", async () => {
         let recordedVideo = document.createElement("video");
         recordedVideo.controls = true;
         recordedVideo.src = URL.createObjectURL(recordedBlob);
-        recordedVideo.classList.add("w-1/4", "rounded-lg");
+        recordedVideo.classList.add("w-1/4", "rounded-lg", "shadow-lg");
 
         let recordHolder = document.getElementById("videos");
         recordHolder.appendChild(recordedVideo);
     };
+}
+
+recordButton.addEventListener("click", async () => {
+    let audio = document.getElementById("audio").checked;
+    startRecording(audio);
 });
